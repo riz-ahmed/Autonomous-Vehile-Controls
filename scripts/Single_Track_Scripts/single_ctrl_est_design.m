a8b62caa703416464b95d883b7416a9709bfe776
rizwanahmed <rizwanahmed@localhost>
2020-09-07 21:34:06 +0200

    script generate SS matrices

--- scripts/Single_Track_Scripts/single_ctrl_est_design.m
+++ scripts/Single_Track_Scripts/single_ctrl_est_design.m
@@ -21,24 +21,39 @@ vx=20;               % Vehicle velocity [m/s]
 
 
 %---------------------------------------------------
-% Enter A and B matrices here
+% vehcile state space model (lienarzied)
 %---------------------------------------------------
-% A=
-% B=
-C=[1 0 0 0 0];
-D=0;
-
-
-%---------------------------------------------------
-% Enter control design parameters here
-%---------------------------------------------------
-%
-%K=
-%kr=
-
+A = [0 1 vx 0 0;
+     0 -(c1 + c2)/m*vx 0 vx-(a*c1-b*c2)/(m*vx) c1/m;
+     0 0 0 1 0;
+     0 -(a*c1-b*c2)/J*vx 0 -(c1*a^2+c2*b^2)/(J*vx) (a*c1)/J;
+     0 0 0 0 -(1/tau)];
+B = [0 0 0 0 (k1/tau)]';
+C =[1 0 0 0 0];
+D =0;
 
 %---------------------------------------------------
-% Enter estimator design parameters here
+% controllability analysis
+%---------------------------------------------------
+Mc = ctrb(A,B);                         % controllability matrix
+det_cont_model = det(Mc);       % the system is controllable
+
+%---------------------------------------------------
+% control design parameters
+%---------------------------------------------------
+% settling time == 2 sec
+% steady-state == 5% within the boundary
+
+% refer to control system notes for more details
+% p_des = (s + p1)^2 + (s + p2)^3, with {p1,p2} = {-3,-9}
+% p_des = s^5 + 33*s^4 + 414*s^3 + 2430*s^2 + 6561*s + 6561
+
+p_des = A^5 + 33*A^4 + 414*A^3 + 2430*A^2 + 6561*A + 6561*eye(5);
+K = [0 0 0 0 1]*inv(Mc)*p_des;
+kr = -1/(C * inv(A - B*K)*B)
+
+%---------------------------------------------------
+% estimator design parameters
 %---------------------------------------------------
 %
 %L=
@@ -47,6 +62,6 @@ D=0;
 %---------------------------------------------------
 % For simulation purposes (not to be modified)
 %---------------------------------------------------
-B1=[B L];                   % Put the B and H matrix together as one matrix B1 (for Simulink implementation purposes) 
-C1=eye(5);                  % Output all state variables from the model
-D1=zeros(5,2);
+% B1=[B L];                   % Put the B and H matrix together as one matrix B1 (for Simulink implementation purposes) 
+% C1=eye(5);                  % Output all state variables from the model
+% D1=zeros(5,2);
